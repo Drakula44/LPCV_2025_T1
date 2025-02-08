33c883ab292cbb0937e4a940e2d4b350d48be6ec
Drakula44 <ninadrakulic04@gmail.com>
2025-02-08 14:23:15 +0100

    Clean coco.py and make it platform independent

--- src/dataset/coco.py
+++ src/dataset/coco.py
@@ -1,17 +1,10 @@
-import subprocess
 import os
-import json
 import shutil
-from tqdm import tqdm
-import torchvision
-
-from .utils import GLOBAL_CLASSES
+import wget
 
 
 def download_coco(dataset_dir: str, validation: bool = False, train: bool = True):
     """Download COCO dataset (Train & Val)"""
-    print("🚀 Downloading COCO dataset...")
-
     urls = {
         "annotations": "http://images.cocodataset.org/annotations/annotations_trainval2017.zip",
     }
@@ -24,100 +17,33 @@ def download_coco(dataset_dir: str, validation: bool = False, train: bool = True
 
     for split, url in urls.items():
         zip_path = os.path.join(dataset_dir, f"{split}.zip")
-        subprocess.run(["wget", "-O", zip_path, url])
-
-        subprocess.run(["7z", "x", zip_path, f"-o{dataset_dir}"])
-        os.remove(zip_path)
-
-    print("✅ COCO dataset downloaded and extracted!")
-
-
-def move_images_to_class_folders(dataset_dir, split, classes):
-    """Move COCO images into class folders based on labels."""
-    annotations_file = os.path.join(
-        dataset_dir, "annotations", f"instances_{split}2017.json"
-    )
-
-    # Load COCO annotations
-    with open(annotations_file, "r") as f:
-        coco_data = json.load(f)
-
-    classes = [cls.lower() for cls in classes]
-    # Map category ID to category name
-    category_map = {cat["id"]: cat["name"] for cat in coco_data["categories"]}
-
-    print(category_map)
-
-    # Keep only the required classes
-    category_map = {k: v for k, v in category_map.items() if v in classes}
-
-    print(category_map)
-
-    filtered_annotations = [
-        ann for ann in coco_data["annotations"] if ann["category_id"] in category_map
-    ]
-
-    if not filtered_annotations:
-        print(f"⚠️ No images found for {split}! Check annotation file.")
-
-    for ann in tqdm(filtered_annotations, desc=f"Moving {split} images"):
-        image_id = ann["image_id"]
-        category_id = ann["category_id"]
-        category_name = category_map[category_id]
-        file_name = next(
-            (img["file_name"] for img in coco_data["images"] if img["id"] == image_id),
-            None,
-        )
-
-        if file_name is None:
-            print(f"⚠️ Warning: No file found for image ID {image_id}")
+        if os.path.exists(zip_path):
+            print(f"Skipping {split} dataset download, as it is already downloaded.")
             continue
-
-        src_path = os.path.join(dataset_dir, f"{split}2017", file_name)
-        dest_folder = os.path.join(dataset_dir, split, category_name)
-        dest_path = os.path.join(dest_folder, file_name)
-
-        # Ensure class folder exists
-        os.makedirs(dest_folder, exist_ok=True)
-
-        # Move the file only if it exists
-        if os.path.exists(src_path):
-            os.rename(src_path, dest_path)
-            print(f"✅ Moved: {src_path} → {dest_path}")
-        else:
-            print(f"⚠️ File missing: {src_path}")
-
-    shutil.rmtree(os.path.join(dataset_dir, f"{split}2017"))
+        print(f"Downloading {split} dataset...")
+        wget.download(url, zip_path)
 
 
-def coco_dataset(filepath):
+def extract_dataset(dataset_dir: str):
+    zip_dir = {
+        "annotations": "annotations",
+        "val": "val2017",
+        "train": "train2017",
+    }
+    for split, dir_name in zip_dir.items():
+        unpack_folder = os.path.join(dataset_dir, dir_name)
+        if os.path.exists(unpack_folder):
+            print(f"Skipping {split} dataset extraction, as it is already extracted.")
+            continue
+        zip_path = os.path.join(dataset_dir, f"{split}.zip")
+        if not os.path.exists(zip_path):
+            print(f"Skipping {split} dataset extraction, as it is not downloaded.")
+            continue
+        print(f"Extracting {split} dataset...")
+        shutil.unpack_archive(zip_path, dataset_dir)
 
-    dataset  = torchvision.datasets.ImageFolder(
-        root = filepath,
-        transform=torchvision.transforms.ToTensor(),
-    )
-    return dataset
 
-
-
-def get_coco(dataset_dir, validation: bool = False, train: bool = True):
-    coco_dir = os.path.join(dataset_dir, "coco")
-    if not os.path.exists(coco_dir):
-        download_coco(coco_dir, validation=validation, train=train)
-        if validation:
-            move_images_to_class_folders(
-                dataset_dir=dataset_dir,
-                split="val",
-                classes=GLOBAL_CLASSES,
-            )
-        if train:
-            move_images_to_class_folders(
-                dataset_dir=dataset_dir,
-                split="train",
-                classes=GLOBAL_CLASSES,
-            )
-    if(validation == True and train == False):
-        return coco_dataset(os.path.join(coco_dir, "val"))
-    elif(validation == False and train == True):
-        return coco_dataset(os.path.join(coco_dir, "train"))
-    return coco_dataset(coco_dir)
+if __name__ == "__main__":
+    dataset_dir = "datasets/coco"
+    download_coco(dataset_dir, validation=True, train=False)
+    extract_dataset(dataset_dir)
